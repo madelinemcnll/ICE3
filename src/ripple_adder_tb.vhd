ed177104006f5216a53e97bd34c91ee358ad433c
madeline.mcnll <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2026-02-13 20:18:34 -0700

    fixed syntax

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,13 +49,13 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-       w_addends <= x"80"; w_Cin <= '0'; wait for 10ns;
+       w_addends <= x"80"; w_Cin <= '0'; wait for 10 ns;
            assert (w_sum = x"8" and w_Cout = '0') report "bad with A3 only" severity failure;
-       w_addends <= x"00"; w_Cin <= '1'; wait for 10ns;
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
            assert (w_sum = x"1" and w_Cout = '0') report "bad with Cin only" severity failure;
-       w_addends <= x"F0"; w_Cin <= '1'; wait for 10ns;
+       w_addends <= x"F0"; w_Cin <= '1'; wait for 10 ns;
            assert (w_sum = x"0" and w_Cout = '1') report "bad with Cin and all A input" severity failure;
-       w_addends <= x"0F"; w_Cin <= '1'; wait for 10ns;
+       w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
            assert (w_sum = x"0" and w_Cout = '1') report "bad with Cin and all B input" severity failure;
 
 		wait; -- wait forever
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -56,11 +56,56 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
+    component full_adder is
+    port (
+        A     : in std_logic;
+        B     : in std_logic;
+        Cin   : in std_logic;
+        S     : out std_logic;
+        Cout  : out std_logic
+        );
+    end component full_adder;
 
     -- declare any signals you will need	
   
 begin
 	-- PORT MAPS --------------------
+    full_adder_0: full_adder
+    port map(
+        A     => A(0),
+        B     => B(0),
+        Cin   => Cin,   -- Directly to input here
+        S     => S(0),
+        Cout  => w_carry(0)
+    );
+
+    full_adder_1: full_adder
+    port map(
+        A     => A(1),
+        B     => B(1),
+        Cin   => w_carry(0),
+        S     => S(1),
+        Cout  => w_carry(1)
+    );
+    
+-- PORT MAPS --------------------
+    full_adder_2: full_adder
+    port map(
+        A     => A(2),
+        B     => B(2),
+        Cin   => w_carry(1),
+        S     => S(2),
+        Cout  => w_carry(2)
+    );
+
+    full_adder_3: full_adder
+    port map(
+        A     => A(3),
+        B     => B(3),
+        Cin   => w_carry(2),
+        S     => S(3),
+        Cout  => Cout
+    );
    
 	---------------------------------
 	
